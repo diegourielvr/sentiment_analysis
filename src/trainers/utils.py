510d3ff8e09b226219638280c453a127b052b75c
diegourielvr <diegourielvr@gmail.com>
2025-04-10 19:11:19 -0600

    Cargar datos desbalanceados o hacer undersampling

--- src/trainers/utils.py
+++ src/trainers/utils.py
@@ -81,51 +81,77 @@ def show_confusion_matrix(y_true, y_pred, title="", path=None, dpi=300):
 def build_datasets(
     data_path: str, text = 'text', label = 'polarity',
     test_size:float = 0.3, val_size:float = 0.0,
-    random_state: int = 42):
+    random_state:int = 42, undersampling:bool = True):
     
     """
-    Si val_dataset=True, se toma el porcentaje especificado val_size
+    Si val_size>0, se toma el porcentaje especificado val_size
     del conjunto de datos de prueba.
     """
     
     df = pd.read_csv(data_path, encoding="utf-8")
     df = df[[text, label]]
 
-    # Separar los datos
-    labels = df[label].unique()
-    df_clases = []
-    for label_name in labels:
-        df_clases.append(df[df[label] == label_name])
-    
-    min_size = min([len(df_clase) for df_clase in df_clases])
-    
-    # Tomar muestras balanceadas
-    df_sampled = []
-    for df_clase in df_clases:
-        df_sampled.append(df_clase.sample(min_size, random_state=random_state))
-    
-    # Unir los datos
-    df_combined = pd.concat(df_sampled)
+    if undersampling:
+        # Separar los datos
+        labels = df[label].unique()
+        df_clases = []
+        for label_name in labels:
+            df_clases.append(df[df[label] == label_name])
+        
+        min_size = min([len(df_clase) for df_clase in df_clases])
+        
+        # Tomar muestras balanceadas
+        df_sampled = []
+        for df_clase in df_clases:
+            df_sampled.append(df_clase.sample(min_size, random_state=random_state))
+        
+        # Unir los datos
+        df_combined = pd.concat(df_sampled)
 
-    # Mezclar datos
-    df_combined = df_combined.sample(frac=1, random_state=random_state).reset_index(drop=True)
-    
-    # Dividr datos
-    train_df, test_df, val_df = None, None, None
-    train_df, test_df = train_test_split(
-        df_combined,
-        test_size=test_size,
-        random_state=random_state,
-        stratify=df_combined[label])
-    
-    if val_size:
-        val_df, test_df = train_test_split(
-            test_df,
-            test_size=val_size,
+        # Mezclar datos
+        df_combined = df_combined.sample(frac=1, random_state=random_state).reset_index(drop=True)
+        
+        # Dividr datos
+        train_df, test_df, val_df = None, None, None
+        train_df, temp_df = train_test_split(
+            df_combined,
+            test_size=test_size,
             random_state=random_state,
-            stratify=test_df[label])
+            stratify=df_combined[label])
+        
+        if val_size:
+            test_df, val_df = train_test_split(
+                temp_df,
+                test_size=val_size,
+                random_state=random_state,
+                stratify=temp_df[label])
+        
+        train_df = train_df.reset_index(drop=True)
+        test_df = test_df.reset_index(drop=True)
+        val_df = val_df.reset_index(drop=True)
+    else:
+        x = df['text'].values
+        y = df['polarity'].values
+        x_train, x_temp, y_train, y_temp = train_test_split(
+            x, y, test_size=test_size,
+            stratify=y, random_state=random_state
+        )
+        x_test, x_val, y_test, y_val = train_test_split(
+            x_temp, y_temp, test_size=val_size,
+            stratify=y_temp, random_state=random_state
+        )
+        train_df = pd.DataFrame({'text': x_train, 'polarity': y_train})
+        test_df = pd.DataFrame({'text': x_test, 'polarity': y_test})
+        val_df = pd.DataFrame({'text': x_val, 'polarity': y_val})
     
-    return train_df.reset_index(drop=True), test_df.reset_index(drop=True), val_df.reset_index(drop=True)
+    total = len(df)
+    p_train = len(train_df) / total * 100
+    p_test = len(test_df) / total * 100
+    p_val = len(val_df) / total * 100
+    print(f"Train: {p_train:.2f}%")
+    print(f"Test: {p_test:.2f}%")
+    print(f"Val: {p_val:.2f}%")
+    return train_df, test_df, val_df 
 
 def save_model(model, path):
     joblib.dump(model, path)
